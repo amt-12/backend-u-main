8052cf4a0b45b20607619501d983f6eec462cebb
AMRIT PAL SINGH <126061621+amt-12@users.noreply.github.com>
2026-05-06 13:12:03 +0530

    Refactor MongoDB connection to use environment variable and update .env configuration

--- config/db.js
+++ config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://amrit0207232_db_user:q7VVID0N2PL8IoD1@ac-xevjxqj-shard-00-00.uc6gevo.mongodb.net:27017,ac-xevjxqj-shard-00-01.uc6gevo.mongodb.net:27017,ac-xevjxqj-shard-00-02.uc6gevo.mongodb.net:27017/?ssl=true&replicaSet=atlas-y1ysj8-shard-0&authSource=admin&appName=job-engine");
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error('Missing environment variable: MONGODB_URI');
+    }
+
+    await mongoose.connect(mongoUri);
 
     console.log("MongoDB Connected");
   } catch (error) {
