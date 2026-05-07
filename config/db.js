04af8b40cc8e2f9b6af1547f1adb70fa3633591f
AMRIT PAL SINGH <126061621+amt-12@users.noreply.github.com>
2026-05-07 15:36:45 +0530

    Update MongoDB connection to use hardcoded URI and add error handling for missing environment variable

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URI;
+    const mongoUri = "mongodb://amrit0207232_db_user:q7VVID0N2PL8IoD1@ac-xevjxqj-shard-00-00.uc6gevo.mongodb.net:27017,ac-xevjxqj-shard-00-01.uc6gevo.mongodb.net:27017,ac-xevjxqj-shard-00-02.uc6gevo.mongodb.net:27017/?ssl=true&replicaSet=atlas-y1ysj8-shard-0&authSource=admin&appName=job-engine";
+    
     if (!mongoUri) {
       throw new Error('Missing environment variable: MONGODB_URI');
     }
