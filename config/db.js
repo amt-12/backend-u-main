c106c773fe5fc9d553f106bfcba3ef9aa7163bf0
AMRIT PAL SINGH <126061621+amt-12@users.noreply.github.com>
2026-05-19 12:29:27 +0530

    Update MongoDB connection string to use SRV format for improved connection handling

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const mongoUri = "mongodb://amrit0207232_db_user:q7VVID0N2PL8IoD1@ac-xevjxqj-shard-00-00.uc6gevo.mongodb.net:27017,ac-xevjxqj-shard-00-01.uc6gevo.mongodb.net:27017,ac-xevjxqj-shard-00-02.uc6gevo.mongodb.net:27017/?ssl=true&replicaSet=atlas-y1ysj8-shard-0&authSource=admin&appName=job-engine";
-    
+    const mongoUri = "mongodb+srv://amrit0207232_db_user:qK6Vp5GdsmICTsKk@unrealstudioz.pvoc22n.mongodb.net/";
+
     if (!mongoUri) {
       throw new Error('Missing environment variable: MONGODB_URI');
     }
